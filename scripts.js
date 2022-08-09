a065b02f1214906ee14d47754b9a4b7d37955d81
adamclear <3589@holbertonschool.com>
2022-08-09 01:41:17 +0000

    updated

--- scripts.js
+++ scripts.js
@@ -49,23 +49,23 @@ function videoPopulate() {
 				}
 				var $videoCarouselItem = $videoCarouselInner.find(`.video${x}`);
 				$videoCarouselItem.append(`<div class="page-wrapper d-flex flex-row justify-content-center">
-																<div class="tutorial-card d-none flex-column d-flex">
-																	<div>
-																		<img class="video_tn img-fluid" src="${result.thumb_url}">
-																		<img class="video_pb img-fluid" src="./images/play.png">
-																	</div>
-																	<div>
-																		<h5 class="font-weight-bold">${result.title}</h5>
-																		<p>${result['sub-title']}</p>
-																	</div>
-																	<div class="d-flex flex-row mb-2">
-																		<img class="tutorial-profile rounded-circle" src="${result.author_pic_url}">
-																		<h5 class="text-purple ml-3">${result.author}</h5>
-																	</div>
-																	<div class="d-flex flex-row star-rating-${x}">
-																	</div>
-																</div>
-															</div>`);
+																		<div class="tutorial-card d-none flex-column d-flex">
+																			<div>
+																				<img class="video_tn img-fluid" src="${result.thumb_url}">
+																				<img class="video_pb img-fluid" src="./images/play.png">
+																			</div>
+																			<div>
+																				<h5 class="font-weight-bold">${result.title}</h5>
+																				<p>${result['sub-title']}</p>
+																			</div>
+																			<div class="d-flex flex-row mb-2">
+																				<img class="tutorial-profile rounded-circle" src="${result.author_pic_url}">
+																				<h5 class="text-purple ml-3">${result.author}</h5>
+																			</div>
+																			<div class="d-flex flex-row star-rating-${x}">
+																			</div>
+																		</div>
+																	</div>`);
 				var $starRating = $videoCarousel.find(`.star-rating-${x}`);
 				for (let y = 1; y <= 5; y++) {
 					if (y <= result.star) {
