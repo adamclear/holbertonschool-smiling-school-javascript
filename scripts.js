d464852af7cf26cd19ca613b9026318f8252a395
adamclear <3589@holbertonschool.com>
2022-08-09 18:41:14 +0000

    updated

--- scripts.js
+++ scripts.js
@@ -1,25 +1,26 @@
 window.onload = () => {
-	carouselPopulate();
-	videoPopulate();
+	setTimeout($('.spinner-border').hide(), 10000);
+	testimonialPopulate();
+	tutorialPopulate();
 }
 
-/* Carousel */
+/* Testimonial */
 
-function carouselPopulate() {
+function testimonialPopulate() {
 
-	var $carousel = $('#testimonialControls');
-	var $carouselInner = $carousel.find('.carousel-inner');
+	var $testimonial = $('#testimonialControls');
+	var $testimonialInner = $testimonial.find('.carousel-inner');
 
-	if ($carouselInner) {
+	if ($testimonialInner) {
 		$.get('https://smileschool-api.hbtn.info/quotes', (result) => {
 			result.forEach((result, x) => {
 				if (x === 0) {
-					$carouselInner.append(`<div class="carousel-item test${x} justify-content-center active"></div>`);
+					$testimonialInner.append(`<div class="carousel-item test${x} justify-content-center active"></div>`);
 				} else {
-					$carouselInner.append(`<div class="carousel-item test${x} justify-content-center"></div>`);
+					$testimonialInner.append(`<div class="carousel-item test${x} justify-content-center"></div>`);
 				}
-				var $carouselItem = $carousel.find(`.test${x}`);
-				$carouselItem.append(`<div class="testimonial d-flex flex-column flex-md-row justify-content-around justify-content-md-center align-items-center">
+				var $testimonialItem = $testimonial.find(`.test${x}`);
+				$testimonialItem.append(`<div class="testimonial d-flex flex-column flex-md-row justify-content-around justify-content-md-center align-items-center">
 															 <img class="rounded-circle mr-5 mb-4 mb-md-0" src="${result.pic_url}" alt="">
 																 <div class="testimonial-text flex-column">
 																	 <p class="mb-4">${result.text}</p>
@@ -28,45 +29,45 @@ function carouselPopulate() {
 																 </div>
 															 </div>`);
 			});
-			$carousel.carousel();
+			$testimonial.carousel();
 		});
 	}
 }
 
 /* Videos */
 
-function videoPopulate() {
-	var $videoCarousel = $('#tutorialControls');
-	var $videoCarouselInner = $videoCarousel.find('.carousel-inner');
+function tutorialPopulate() {
+	var $tutorialCarousel = $('#tutorialControls');
+	var $tutorialCarouselInner = $tutorialCarousel.find('.carousel-inner');
 
-	if ($videoCarouselInner) {
+	if ($tutorialCarouselInner) {
 		$.get('https://smileschool-api.hbtn.info/popular-tutorials', (result) => {
 			result.forEach((result, x) => {
 				if (x === 0) {
-					$videoCarouselInner.append(`<div class="carousel-item video${x} active"></div>`);
+					$tutorialCarouselInner.append(`<div class="carousel-item tutorial${x} active"></div>`);
 				} else {
-					$videoCarouselInner.append(`<div class="carousel-item video${x}"></div>`);
+					$tutorialCarouselInner.append(`<div class="carousel-item tutorial${x}"></div>`);
 				}
-				var $videoCarouselItem = $videoCarouselInner.find(`.video${x}`);
-				$videoCarouselItem.append(`<div class="page-wrapper d-flex flex-row justify-content-center">
-																		<div class="tutorial-card d-none flex-column d-flex">
-																			<div>
-																				<img class="video_tn img-fluid" src="${result.thumb_url}">
-																				<img class="video_pb img-fluid" src="./images/play.png">
-																			</div>
-																			<div>
-																				<h5 class="font-weight-bold">${result.title}</h5>
-																				<p>${result['sub-title']}</p>
-																			</div>
-																			<div class="d-flex flex-row mb-2">
-																				<img class="tutorial-profile rounded-circle" src="${result.author_pic_url}">
-																				<h5 class="text-purple ml-3">${result.author}</h5>
-																			</div>
-																			<div class="d-flex flex-row star-rating-${x}">
-																			</div>
-																		</div>
-																	</div>`);
-				var $starRating = $videoCarousel.find(`.star-rating-${x}`);
+				var $tutorialCarouselItem = $tutorialCarouselInner.find(`.tutorial${x}`);
+				$tutorialCarouselItem.append(`<div class="page-wrapper d-flex flex-row justify-content-center">
+																				<div class="tutorial-card d-none flex-column d-flex">
+																					<div>
+																						<img class="video_tn img-fluid" src="${result.thumb_url}">
+																						<img class="video_pb img-fluid" src="./images/play.png">
+																					</div>
+																					<div>
+																						<h5 class="font-weight-bold">${result.title}</h5>
+																						<p>${result['sub-title']}</p>
+																					</div>
+																					<div class="d-flex flex-row mb-2">
+																						<img class="tutorial-profile rounded-circle" src="${result.author_pic_url}">
+																						<h5 class="text-purple ml-3">${result.author}</h5>
+																					</div>
+																					<div class="d-flex flex-row star-rating-${x}">
+																					</div>
+																				</div>
+																			</div>`);
+				var $starRating = $tutorialCarousel.find(`.star-rating-${x}`);
 				for (let y = 1; y <= 5; y++) {
 					if (y <= result.star) {
 						$starRating.append(`<img class="star-rating mr-1" src="./images/star_on.png">`);
@@ -76,7 +77,25 @@ function videoPopulate() {
 				}
 				$starRating.append(`<p class="duration text-purple">${result.duration}</p>`);
 			});
-			$videoCarousel.carousel();
+			$('.tutorials .carousel .carousel-item').each(function() {
+					var minPerSlide = 4;
+					var next = $(this).next();
+					if (!next.length) {
+					next = $(this).siblings(':first');
+					}
+					next.children(':first-child').clone().appendTo($(this));
+					
+					for (var i=0;i<minPerSlide;i++) {
+						next=next.next();
+						if (!next.length) {
+								next = $(this).siblings(':first');
+							}
+						
+						next.children(':first-child').clone().appendTo($(this));
+					}
+			});
+			$tutorialCarousel.carousel();
 		});
 	}
+	$(".loader").hide();
 }
